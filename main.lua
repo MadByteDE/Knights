0d90b497598196b4f5c2c7ce75b27b143b2404d7
Lars Loe <madbyte@tuta.io>
2021-11-20 21:41:27 +0100

    Moved images // minor changes

--- main.lua
+++ main.lua
@@ -1,1 +1,27 @@
- 
+
+_TITLE          = "Knights"
+_VERSION        = 'v0.0.1'
+_DESCRIPTION    = '2D vs/coop dungeon-crawler for 1 - 4 players'
+_URL            = 'https://github.com/MadByteDE/Knights'
+
+local Game = require("game")
+
+function love.load()
+    Game:init()
+end
+
+function love.update(dt)
+    Game:update(dt)
+end
+
+function love.draw()
+    Game:draw()
+end
+
+function love.keypressed(key)
+    Game:keypressed(key)
+end
+
+function love.exit()
+    Game:exit()
+end
