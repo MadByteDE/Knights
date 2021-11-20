22ac403fbd8dc7605e5ef20a297c4a5a1db7f542
Lars Loe <madbyte@tuta.io>
2021-11-20 21:13:24 +0100

    Rename love icon

---
+++ game.lua
@@ -0,0 +1,20 @@
+
+
+local Game = {}
+
+function Game:init()
+end
+
+function Game:update(dt)
+end
+
+function Game:draw()
+end
+
+function Game:keypressed(key)
+end
+
+function Game:exit()
+end
+
+return Game
