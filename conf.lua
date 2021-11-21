6cb31da605e2b11b55154ef488914d2070aca947
Lars Loe <madbyte@tuta.io>
2021-11-21 13:54:01 +0100

    Added  conf.lua

---
+++ conf.lua
@@ -0,0 +1,10 @@
+
+function love.conf(t)
+    t.window.title = "Knights"
+    t.identity = "knights"
+    t.modules.joystick = false
+    t.modules.physics = false
+    t.modules.video = false
+    t.window.resizable = false
+    t.window.vsync = 1
+end
